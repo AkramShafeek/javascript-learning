e59f885c1eba4231f419ed649d6a191ecf78ee5f
Akram <akramshafeek70@gmail.com>
2023-04-06 23:58:39 +0530

    Added sliding animation to carousel

--- Timeline image carousel/script.js
+++ Timeline image carousel/script.js
@@ -11,10 +11,11 @@ function switchImage() {
     let progressContainer = document.getElementById('progressBarContainer');
     let progressLine = document.getElementById('progressLine');
     let img = document.getElementById('carouselImages');
-
+    
     progressLine.remove();
     progressLine.style.width = '1.5%';
     progressContainer.append(progressLine);
+    img.style.transform = 'rotateY(-45deg) translate(-90%)' 
     img.style.opacity = '0';
     
     setTimeout(() => {
@@ -23,8 +24,14 @@ function switchImage() {
 
     setTimeout(() => {
         img.src = './images/' + imgName;
-        img.style.opacity = '1';
+        img.style.transform = 'rotateY(45deg) translate(90%)' 
     }, 500);
+    
+    setTimeout(() => {
+        img.style.opacity = '1';
+        img.style.transform = 'rotateY(0deg) translate(0%)' 
+    }, 700);
+
 
     index = (index + 1) % numberOfImages;
 }
