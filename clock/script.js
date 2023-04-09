cb0ac5079229f371f5c9d1e8a732be31499e3393
Akram <akramshafeek70@gmail.com>
2023-04-09 06:33:37 +0530

    Added clock functionality

--- clock/script.js
+++ clock/script.js
@@ -1,14 +1,57 @@
-let hand = document.getElementById('secondsHand');
-let clockContainer = document.getElementById('handsContainer');
-let i = 0;
+let secondsHand = document.getElementById('secondsHand');
+let minutesHand = document.getElementById('minutesHand');
+let hoursHand = document.getElementById('hoursHand');
+
+let handsContainer = document.getElementById('handsContainer');
+
+var i;
+var time = new Date();
+var hours = time.getHours() % 12;
+var minutes = time.getMinutes();
+var seconds = time.getSeconds();
+
+function initializeClock(){
+    i = seconds
+    secondsHand.style.transform = `translateY(-50%) rotate(${seconds*6}deg)`;
+    minutesHand.style.transform = `translateY(-50%) rotate(${minutes*6 + 0.1*seconds}deg)`;
+    hoursHand.style.transform = `translateY(-50%) rotate(${hours*30 + 0.5*minutes + (1/120)*seconds}deg)`;
+
+}
+
+initializeClock();
+
 setInterval(()=>{
-    if(i === 0){
-        hand.remove();
-        hand.style.transform = `translateY(-50%) rotate(${i*6}deg)`;
-        clockContainer.append(hand);
+    seconds++;
+
+    secondsHand.style.transform = `translateY(-50%) rotate(${seconds*6}deg)`;
+    minutesHand.style.transform = `translateY(-50%) rotate(${minutes*6 + seconds*(1/10)}deg)`;
+    hoursHand.style.transform = `translateY(-50%) rotate(${hours*30 + + 0.5*minutes + seconds*(1/120)}deg)`;
+
+    if(seconds == 60){
+        seconds = 0;
+        setTimeout(()=>{
+            secondsHand.remove();
+            secondsHand.style.transform = `translateY(-50%) rotate(${seconds*6}deg)`;
+            handsContainer.append(secondsHand);
+        },100)
+        minutes++;
     }
-    else
-        hand.style.transform = `translateY(-50%) rotate(${i*6}deg)`;
-        
-    i = (i + 1) % 60
+    if(minutes == 60){
+        minutes = 0;
+        setTimeout(()=>{
+            minutesHand.remove();
+            minutesHand.style.transform = `translateY(-50%) rotate(${minutes*6 + seconds*(1/10)}deg)`;
+            handsContainer.append(minutesHand);
+        },100)
+        hours++;
+    }
+    if(hours == 12){
+        setTimeout(()=>{
+            hoursHand.remove();
+            hoursHand.style.transform = `translateY(-50%) rotate(${hours*30 + + 0.5*minutes + seconds*(1/120)}deg)`;
+            handsContainer.append(hoursHand);
+        },100)
+        hours = 0;
+    }
+    
 },1000)
